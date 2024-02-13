00fd1fb62f58b9b51f7a8a19807ba4e4f31da74b
rashidpbi <rashidpbi786@gmail.com>
2024-02-13 09:52:10 +0530

    simple fragment added to add key

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,9 +1,15 @@
+import { Fragment } from "react";
 function Table({ data, config ,keyFn ,keyFnVeh,vehicleData,vehicleConfig  }) {
-    const renderedHeaders = vehicleConfig.map((item) => (
+    const renderedHeaders = vehicleConfig.map((item) => {
+       
+        if (item.header){
+                return <Fragment key={item.label}>{item.header()}</Fragment>;
+        }
+        return(
         <th className="p-3" key={item.label}>
             {item.label.charAt(0).toUpperCase() + item.label.slice(1)}
-        </th>
-    ));
+        </th>)
+});
 
     const renderedRows = vehicleData.map((rawData) => {
         const renderedCells = vehicleConfig.map((vehicleConfig) => (
