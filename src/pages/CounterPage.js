48317aa812de30e4f5fa34527c3a1cc9fd829e7f
rashidpbi <rashidpbi786@gmail.com>
2024-02-17 12:20:20 +0530

    modified counter using useReducer

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -1,34 +1,84 @@
 import Panel from '../components/Panel'
 import Button from '../components/Button';
-import { useState } from 'react';
+import { useReducer } from 'react';
 
 
 
 function CounterPage({ initialState }) {
-const [count,setCount] = useState(initialState);
-const [ valueToAdd,setValueToAdd] = useState(0)
+// const [count,setCount] = useState(initialState);
+// const [ valueToAdd,setValueToAdd] = useState(0)
+const ACTIONS = {
+  INCREMENT: "increment",
+  DECREMENT: "decrement",
+  SUBMIT:"submit",
+  CHANGE:"value-change",
+}
+function reducer(state,action){
+switch (action.type) {
+case  ACTIONS.INCREMENT:
+  return {
+   ...state,
+   count:state.count + 1
+  }
+case  ACTIONS.DECREMENT:
+  return {
+    ...state,
+    count: state.count - 1
+  }
+case  "value-change":
+  return {
+    ...state,
+    valueToAdd: action.payload
+  }
+case  "submit":
+  return {
+    ...state,
+    count: state.count +  state.valueToAdd,
+    valueToAdd: 0,
+  }
+
+  
+  default: 
+  return state;
+}
+
+}
+const [state,dispatch] = useReducer(reducer,{
+  count: 0,
+  valueToAdd:initialState,
+})
 const onIncrement = ()=>{
-  setCount(count + 1)
+  // setCount(count + 1)
+  dispatch({
+    type:  ACTIONS.INCREMENT
+  });
 }
 const onDecrement = ()=>{
-  setCount(count - 1)
+  dispatch({
+    type:  ACTIONS.DECREMENT
+  });
 }
 
 const handleSubmit = (e)=>{
 e.preventDefault();
-setCount(count + valueToAdd)
-setValueToAdd(0)
+dispatch({
+  type:  ACTIONS.SUBMIT
+});
+
 
 }
 const handleChange = (e)=>{
   const value = parseInt(e.target.value) ||0;
-  console.log(typeof value)
- setValueToAdd(value);
+  dispatch({
+    type:  ACTIONS.CHANGE,
+    payload: value
+  });
+//  setValueToAdd(value);
 
 }
   return (
     <Panel className="m-3">
-      <h1 className="text-lg">value of count is {count}</h1>
+      <h1 className="text-lg">value of count is {state.count}</h1>
       
       <div className="flex flex-row">
         <Button onClick={onIncrement}>Increment</Button>
@@ -36,7 +86,7 @@ const handleChange = (e)=>{
       </div>
       <form onSubmit={handleSubmit}>
         <label >Add a lot!</label><br />
-        <input className='p-1 m-3 bg-gray-50 border border-gray-300'  value={valueToAdd || ''} onChange={handleChange}  type="number"/><br /><br />
+        <input className='p-1 m-3 bg-gray-50 border border-gray-300'  value={state.valueToAdd || ''} onChange={handleChange}  type="number"/><br /><br />
         <Button>Add it</Button>
         
       </form>
